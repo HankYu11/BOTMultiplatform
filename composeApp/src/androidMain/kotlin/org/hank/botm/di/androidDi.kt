34111429713f328d7ed05b637c3a9594db227de2
Hank <hank@younow.com>
2025-06-19 16:08:16 +0800

    Move platform modules

--- composeApp/src/androidMain/kotlin/org/hank/botm/di/androidDi.kt
+++ composeApp/src/androidMain/kotlin/org/hank/botm/di/androidDi.kt
@@ -1,7 +1,10 @@
-package org.hank.botm
+package org.hank.botm.di
 
 import io.ktor.client.HttpClient
+import org.hank.botm.createHttpClient
 import org.hank.botm.data.database.AppDatabase
+import org.hank.botm.getAppDatabase
+import org.hank.botm.getDatabaseBuilder
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
